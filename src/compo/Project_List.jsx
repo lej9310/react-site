435455bcb21a0ca7bda3e836c6b10b954e76783f
lej9310 <lej9310@gmail.com>
2025-08-19 10:04:01 +0900

    8/19

--- src/compo/Project_List.jsx
+++ src/compo/Project_List.jsx
@@ -3,16 +3,33 @@ import { Link, Outlet } from "react-router-dom"
 function Project_List() {
 
     return (
-        <>            
+        <>
             {/* 하위 라우트로 이동할 링크 */}
-            <ul>
-                <li>
-                    <Link to="D04P1">Project 1</Link>{' '}
-                </li>
-                <li>
-                    <Link to="D04P2">Project 2</Link>{' '}
-                </li>
-            </ul>
+            <div class="table">
+                <table>
+                    <tr class="head">
+                        <td>DAY</td>
+                        <td>Project</td>                        
+                    </tr>
+
+                    <tr>
+                        <td>DAY04</td>
+                        <td>
+                            <ul>
+                                <li>
+                                    <Link to="D04P1">Project 1</Link>{' '}
+                                </li>
+                                <li>
+                                    <Link to="D04P2">Project 2</Link>{' '}
+                                </li>
+                            </ul>
+                        </td>
+                    </tr>
+
+
+                </table>
+            </div>
+
 
             {/* 하위 컴포넌트 렌더링 위치 */}
             <Outlet />
