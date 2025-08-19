957c881db2a69c8d5a349d041b0a0071ef60c884
lej9310 <lej9310@gmail.com>
2025-08-19 10:25:13 +0900

    8/19

--- src/compo/Project_List.jsx
+++ src/compo/Project_List.jsx
@@ -5,15 +5,77 @@ function Project_List() {
     return (
         <>
             {/* 하위 라우트로 이동할 링크 */}
-            <div class="table">
+            <div>
                 <table>
-                    <tr class="head">
+                    <tr className="head">
                         <td>DAY</td>
+                        <td>DATE</td>
                         <td>Project</td>                        
+                    </tr>
+
+                     <tr>
+                        <td>DAY01</td>
+                        <td>08.07.(목)</td>
+                        <td>
+                            <ul>
+                                <li>
+                                    <Link to="D04P1">채워야함</Link>{' '}
+                                </li>
+                                <li>
+                                    <Link to="D04P2">채워야함</Link>{' '}
+                                </li>
+                            </ul>
+                        </td>
+                    </tr>
+
+                     <tr>
+                        <td>DAY02</td>
+                        <td>08.08.(금)</td>
+                        <td>
+                            <ul>
+                                <li>
+                                    <Link to="D04P1">채워야함</Link>{' '}
+                                </li>
+                                <li>
+                                    <Link to="D04P2">채워야함</Link>{' '}
+                                </li>
+                            </ul>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>DAY03</td>
+                        <td>08.11.(월)</td>
+                        <td>
+                            <ul>
+                                <li>
+                                    <Link to="D04P1">채워야함</Link>{' '}
+                                </li>
+                                <li>
+                                    <Link to="D04P2">채워야함</Link>{' '}
+                                </li>
+                            </ul>
+                        </td>
                     </tr>
 
                     <tr>
                         <td>DAY04</td>
+                        <td>08.12.(화)</td>
+                        <td>
+                            <ul>
+                                <li>
+                                    <Link to="D04P1">Component 생성: Counter, InputMirror, ColorChange, TodoList</Link>{' '}
+                                </li>
+                                <li>
+                                    <Link to="D04P2">Hook: UseEffect </Link>{' '}
+                                </li>
+                            </ul>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>DAY05</td>
+                        <td>08.13.(수)</td>
                         <td>
                             <ul>
                                 <li>
@@ -25,7 +87,6 @@ function Project_List() {
                             </ul>
                         </td>
                     </tr>
-
 
                 </table>
             </div>
