7693c5ef01f3786f70a01bbc1dacd89f463fcdf9
lej9310 <lej9310@gmail.com>
2025-08-21 10:21:11 +0900

    8/21

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import './App.css'
+//  ===============================
 import Signin from "./compo/Signin"
 import Home from "./compo/Home"
 import Layout from "./compo/Layout"
@@ -20,39 +21,32 @@ import PostWritePage from "./day/day07_0818/p02/components/pages/PostWritePage"
 function App() {
 
   return (
-    <>
-      {/* URL 경로에 따라 다른 컴포넌트 렌더링 */}
-      <BrowserRouter>
-        <Routes>
-          {/* Layout을 최상위 요소로 설정, 자식 라우트는 모두 여기에 포함 */}
-          <Route path="/react-site/" element={<Layout />} />
-           {/* index는 /react-site/로 접속 시 Home 출력 */}
+    <BrowserRouter basename="/react-site/">
+      <Routes>
+        {/* Layout을 최상위 요소로 설정 */}
+        <Route path="/" element={<Layout />} >
           <Route index element={<Home />} />
-          <Route path="/react-site/Signin/" element={<Signin />} />
+          <Route path="Signin" element={<Signin />} />
+          <Route path="project_List" element={<Project_List />} />
+        </Route>
+        
+        {/* 하위 페이지: Layout 내부에서 Outlet으로 렌더링 */}
+        <Route path="project_List/D04P1" element={<D04P1 />} />
+        <Route path="project_List/D04P2/" element={<D04P2 />} />
+        <Route path="project_List/D05P1/" element={<D05P1 />} />
+        <Route path="project_List/D05P2/" element={<D05P2 />} />
+        <Route path="project_List/D06P1/" element={<D06P1 />} />
+        <Route path="project_List/D06P2/" element={<D06P2 />} />
+        <Route path="project_List/D06P3/" element={<D06P3 />} />
+        <Route path="project_List/D07P1/" element={<D07P1 />} />
+        <Route path="project_List/D07P2/*" element={<D07P2 />} />
 
-           {/* 하위 페이지들은 모두 Layout 내부에서 Outlet으로 렌더링 */}
-          <Route path="/react-site/project_List/" element={<Project_List />} />
+        {/* 루트에서 직접 접근 가능한 postId 경로 추가 */}
+        <Route path="/post/:postId" element={<PostViewPage />} />
+        <Route path="/post-write" element={<PostWritePage />} />
 
-          <Route path="/react-site/project_List/D04P1/" element={<D04P1 />} />
-          <Route path="/react-site/project_List/D04P2/" element={<D04P2 />} />
-
-          <Route path="/react-site/project_List/D05P1/" element={<D05P1 />} />
-          <Route path="/react-site/project_List/D05P2/" element={<D05P2 />} />
-
-          <Route path="/react-site/project_List/D06P1/" element={<D06P1 />} />
-          <Route path="/react-site/project_List/D06P2/" element={<D06P2 />} />
-          <Route path="/react-site/project_List/D06P3/" element={<D06P3 />} />
-
-          <Route path="/react-site/project_List/D07P1/" element={<D07P1 />} />
-          {/* 와일드카드(*)를 넣어 하위 모든 경로도 포함해서 매칭 */}
-          <Route path="/react-site/project_List/D07P2/*" element={<D07P2 />} />
-          {/* 루트에서 직접 접근 가능한 postId 경로 추가 */}
-          <Route path="/post/:postId" element={<PostViewPage />} />
-          <Route path="post-write" element={<PostWritePage />} />
-
-        </Routes>
-      </BrowserRouter>
-    </>
+      </Routes>
+    </BrowserRouter >
   )
 }
 
--- src/compo/Layout.jsx
+++ src/compo/Layout.jsx
@@ -4,29 +4,24 @@ import react_logo from '../image/react_logo.png';
 import "../App.css"
 
 function Layout() {
-
     return (
         <>
             <header className="top">
                 <img src={react_logo} alt="로고" className="react_logo" />
-
                 <nav className="topnav">
-                    <Link to="/react-site/project_List/" className="item btn">Project List</Link>
-                    <Link to="/react-site/" className="item btn">Home</Link>
-                    <Link to="/react-site/signin/" className="item Signin_btn">Sign in</Link>
+                    <Link to="project_List/" className="item btn">Project List</Link>
+                    <Link to="/" className="item btn">Home</Link>
+                    <Link to="signin/" className="item Signin_btn">Sign in</Link>
                 </nav>
             </header>
 
             <main>
-                <Outlet />
-                <h2>React Main Home</h2>
-
+                <Outlet />                
                 <div className="img_wrap">
                     <img src={project_img} alt="project image"></img>
                     <div className="text_on_img">My React List</div>
                 </div>
             </main>
-
         </>
     )
 }
